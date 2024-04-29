28b15a2d9fecbabb0e0bdb1f3f166bf90bd3dada
Brayan Espinoza <bjespinozah@gmail.com>
2024-04-28 21:33:26 -0500

    Move dtos to specific package

--- src/main/java/org/example/adapters/controllers/HtmxController.java
+++ src/main/java/org/example/adapters/controllers/HtmxController.java
@@ -1,8 +1,16 @@
 package org.example.adapters.controllers;
 
+import org.example.adapters.dtos.RestDTOs;
+import org.example.interfaces.services.ProductService;
+import org.example.mapper.AppMapper;
+import org.example.usecases.ProductUseCase;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.swing.*;
 import java.util.Arrays;
@@ -10,9 +18,16 @@ import java.util.Arrays;
 @Controller
 public class HtmxController {
 
-    public record Product(Integer id, String name, Boolean presentationInBox, Integer quantity){}
 
-    public record Item (String name, Integer numberOfBoxes, Integer unitsPerBox, Integer units, Integer total){}
+    private AppMapper appMapper;
+
+    private ProductService productService;
+
+    @Autowired
+    public HtmxController(AppMapper appMapper, ProductService productService) {
+        this.appMapper = appMapper;
+        this.productService = productService;
+    }
 
     @GetMapping(path = "/home")
     public String getHome(){
@@ -21,14 +36,22 @@ public class HtmxController {
 
     @GetMapping(path = "/products")
     public String getProducts(Model model){
-        var product = new Product(1, "Panadol", true, 23);
-        model.addAttribute("products", Arrays.asList(product));
+        var products = productService.findAll()
+                .stream()
+                .map(appMapper::productUseCaseToProductController)
+                .toList();
+        model.addAttribute("products", products);
         return "products";
     }
 
+    @PostMapping("/products")
+    public String create(@RequestBody RestDTOs.Product product) {
+        productService.create(appMapper.productControllerToProductUseCase(product));
+        return "redirect:/products";
+    }
     @GetMapping(path = "/inventory")
     public String getInventory(Model model){
-        var item = new Item("Panadol", 1, 10, 5, 10);
+        var item = new RestDTOs.Item("Panadol", 1, 10, 5, 10);
         model.addAttribute("inventory", Arrays.asList(item));
         return "inventory";
     }
--- src/main/java/org/example/mapper/AppMapper.java
+++ src/main/java/org/example/mapper/AppMapper.java
@@ -1,17 +1,18 @@
 package org.example.mapper;
 import org.example.adapters.controllers.ProductController;
+import org.example.adapters.dtos.RestDTOs;
 import org.example.entities.Product;
 import org.example.usecases.ProductUseCase;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface AppMapper {
-    ProductUseCase.Product productControllerToProductUseCase(ProductController.Product product);
+    ProductUseCase.Product productControllerToProductUseCase(RestDTOs.Product product);
 
     Product productUseCaseToProductEntity(ProductUseCase.Product product);
 
     ProductUseCase.Product productEntityToProductUseCase(Product product);
 
-    ProductController.Product productUseCaseToProductController(ProductUseCase.Product product);
+    RestDTOs.Product productUseCaseToProductController(ProductUseCase.Product product);
 
 }
